42d6e86dd8f0e5142d5aee3d67ed29db41b0c67a
Learner <1181434433@qq.com>
2022-04-30 22:35:39 +0800

    "b1测试"

--- dome1/src/main/java/com/DLY/enumTest/Text.java
+++ dome1/src/main/java/com/DLY/enumTest/Text.java
@@ -1,11 +1,14 @@
 package com.DLY.enumTest;
 
+import java.util.HashMap;
+
 public enum Text {
     ONE,TOW;
     private String str=null;
     private Text(){
         System.out.println("私有的无参构造运行");
         str = new String("我还是我吗");
+        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
     }
     public String getStr(){
         return str;
